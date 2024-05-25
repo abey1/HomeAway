787924cee0cda190e3f9afbc0aea09b034e447d2
abey <bruckabey@gmail.com>
2024-05-25 12:34:35 +0930

    boilerplate has been removed and chadcn/ui installed, started using button

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,26 @@
+"use client";
+import { Button, buttonVariants } from "@/components/ui/button";
+import Link from "next/link";
+
 function HomePage() {
-  return <h3 className="text-3xl">HomePage</h3>;
+  const buttonStyle = "capitalize m-8";
+  return (
+    <div>
+      <h3 className="text-3xl">HomePage</h3>
+      <Button
+        size="default"
+        className={buttonStyle}
+        onClick={() => {
+          console.log("i am clicked");
+        }}
+      >
+        Button
+      </Button>
+      <Button asChild variant="ghost">
+        <Link href="/properties">properties</Link>
+      </Button>
+    </div>
+  );
 }
 
 export default HomePage;
